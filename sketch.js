fac21d0e3624a9510f5e631c489a1c3cacd01d47
diyujain26 <67596731+diyujain26@users.noreply.github.com>
2020-07-29 06:21:36 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,65 +1,36 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
+class Box {
 
-function preload()
-{
+  constructor(x, y, width, height) {
 
-	helicopterIMG=loadImage("helicopter.png")
-	packageIMG=loadImage("package.png")
+    var options = {
 
-}
+        'restitution':0.8,
+        'friction':0.3,
+        'density':1.0
 
-function setup() {
+    }
 
-	createCanvas(800, 700);
+    this.body = Bodies.rectangle(x, y, width, height, options);
 
-	rectMode(CENTER);
-
-	packageSprite=createSprite(width/2, 600, 10,10);
-	packageSprite.addImage(packageIMG)
-	packageSprite.scale=0.2
-
-	helicopterSprite=createSprite(width/2, 200, 10,10);
-	helicopterSprite.addImage(helicopterIMG)
-	helicopterSprite.scale=0.6
-
-	groundSprite=createSprite(width/2, height-35, width,5);
-	groundSprite.shapeColor=color(255)
-
-	engine = Engine.create();
-	world = engine.world;
-
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
-	World.add(world, packageBody);
-
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-	 Engine.run(engine);
-  
-}
-
-function draw() {
-  rectMode(CENTER);
-
-  background(0);
-  Engine.update(engine);
-
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y
-
-  drawSprites();
- 
-}
-
-function keyPressed() {
- if (keyCode === DOWN_ARROW) {
-	 Matter.Body.setStatic(packageBody,false)
+    this.width = width;
+    this.height = height;
     
+    World.add(world, this.body);
+
   }
-}
+
+  display(){
+
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x,pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill(255);
+    rect(0,0, this.width, this.height);
+
+    pop();
+
+  }
+};
